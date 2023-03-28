30f8c9bc9e706c02cc2943bae99906a1f897de17
R <a34977@outlook.com>
2023-03-28 20:39:22 +0800

    更新头像信息

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,10 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      // avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
+      avatar: 'https://img2.baidu.com/it/u=1885222319,1935280267&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
+      name: 'catNamedDog',
+      description: 'Base on <a href="https://platform.openai.com/" class="text-blue-500" target="_blank" >OpenAI</a>',
     },
   }
 }
